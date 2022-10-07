ddecb77abc0df5e480dd30435e8b139e166a8ab5
KorotkayaNastuha <skiteikina11@gmail.com>
2022-10-07 19:46:31 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const markUp = galleryItems.map((image) => 
+	`<div class="gallery__item">
+		<a class="gallery__link" href=${image.original}>
+		<img class = "gallery__image"
+			src=${image.preview}
+			data-sourse=${image.original}
+			alt=${image.description}
+		/>
+		</a>
+	</div >`).join("");
+
+galleryEl.innerHTML = markUp;
+
+    new SimpleLightbox('.gallery a', {
+	captionDelay: 250, captiosnData: "alt"
+})
